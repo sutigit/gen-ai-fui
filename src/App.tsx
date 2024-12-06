f37abd83597aca1298b2c4db3d2d5389a764a0ef
sutigit <panyanilben@gmail.com>
2024-12-06 21:59:29 +0200

    init layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
-import { useEffect, useState } from 'react'
+import './App.css'
 
+interface TextInputProps {
+  placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  style?: React.CSSProperties;
+}
 
+const TextInput: React.FC<TextInputProps> = ({ placeholder, value, onChange }) => (
+  <div className='app-main-form-container'>
+    <form className='app-main-form' onSubmit={() => console.log("submit")}>
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+      <button
+        type="submit">
+        Send
+      </button>
+    </form>
+  </div>
+);
 
 function App() {
-  const [elements, setElements] = useState<HTMLElement[]>([])
-
-  useEffect(() => {
-
-    setElements([])
-  }, [])
 
   return (
     <main>
+      <TextInput
+        placeholder="Type something..."
+        value="Hello, World!"
+        onChange={(e) => console.log(e.target.value)} />
     </main>
   )
 }
